6df7ef2f199a4edef4043f1fce7445a19c7e7f6f
abdul mueed <abdul.mueed.shahbaz@redmath.com>
2024-12-11 17:26:53 +0500

    changes

--- smartcontract/contract-gateway/src/main/java/com/abdul/legalcontract/domain/hyperledger/usecase/OffChainDataListenerUseCaseImpl.java
+++ smartcontract/contract-gateway/src/main/java/com/abdul/legalcontract/domain/hyperledger/usecase/OffChainDataListenerUseCaseImpl.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.hyperledger.fabric.client.Checkpointer;
 import org.hyperledger.fabric.client.Network;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -16,23 +18,24 @@ import java.io.IOException;
 @Slf4j
 @RequiredArgsConstructor
 public class OffChainDataListenerUseCaseImpl implements OffChainDataListenerUseCase {
+
     private static final long START_BLOCK = 0L;
 
-    private final Gson GSON;
     private final Network network;
     private final Checkpointer fileCheckpointer;
     private final BlockParserUseCase blockParserUseCase;
     private final ProcessBlockUseCase processBlockUseCase;
 
     @Override
-    public void execute() throws IOException {
+    public void execute() {
         log.info("Starting event listening from block {}",
                 Long.toUnsignedString(fileCheckpointer.getBlockNumber().orElse(START_BLOCK)));
 
         log.info(fileCheckpointer.getTransactionId().map(transactionId -> "Last processed transaction ID within block: "
                 + transactionId).orElse("No last processed transaction ID"));
 
-        var blocks = network.newBlockEventsRequest().startBlock(START_BLOCK) // Used only if there is no checkpoint block number
+        var blocks = network.newBlockEventsRequest()
+                .startBlock(START_BLOCK) // Used only if there is no checkpoint block number
                 .checkpoint(fileCheckpointer).build().getEvents();
 
         blocks.forEachRemaining(blockProto -> {
